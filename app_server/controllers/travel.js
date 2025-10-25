0b60eafa53c615be4f53e2de03a5dce0a6448dc4
Johnathan Dumke <johnathan.dumke@snhu.edu>
2025-10-24 22:43:55 -0700

    Module 3 completed baseline

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,3 +1,13 @@
+var fs = require('fs');
+
+// read JSON at module load 
+var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
+
 exports.travel = (req, res) => {
-  res.render('travel', { title: 'Travel' });
+  // teacher pattern: pass the list to the view
+  res.render('travel', { title: 'Travlr Getaways', trips: list });
 };
+
+
+
+
